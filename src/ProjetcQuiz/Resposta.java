7068468cb0ba339af4db33b0eb353b724634bcb8
JClaion <Joseclaionmartins@gmail.com>
2023-10-16 14:59:51 -0400

    Classe Resposta adicionada

--- src/ProjetcQuiz/Resposta.java
+++ src/ProjetcQuiz/Resposta.java
@@ -7,8 +7,42 @@ public class Resposta {
 	private boolean acertou;
 	private int pontuacaoPergunta;
 	
-	boolean VerificarResposta(Pergunta pergunta, int alternativa){
-		this.acertou = pergunta.alternativa[alternativa];
+	void VerificarResposta(Pergunta pergunta, int alternativa){
+		Alternativa resp = pergunta.getAlternativas(alternativa);
+		this.acertou = resp.getCorreta();
 	}
+
+	public Jogador getJogador() {
+		return jogador;
+	}
+
+	public void setJogador(Jogador jogador) {
+		this.jogador = jogador;
+	}
+
+	public Pergunta getPergunta() {
+		return pergunta;
+	}
+
+	public void setPergunta(Pergunta pergunta) {
+		this.pergunta = pergunta;
+	}
+
+	public boolean getAcertou() {
+		return acertou;
+	}
+
+	public void setAcertou(boolean acertou) {
+		this.acertou = acertou;
+	}
+
+	public int getPontuacaoPergunta() {
+		return pontuacaoPergunta;
+	}
+
+	public void setPontuacaoPergunta(int pontuacaoPergunta) {
+		this.pontuacaoPergunta = pontuacaoPergunta;
+	}
+	
 
 }
